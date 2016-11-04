a8ec43eb45c7cbb9e06cecb6ff5e263b73b873a2
kang yoosam <yooska14@gmail.com>
2016-11-05 06:09:23 +0900

    [add]Animation fires when press space key

--- Assets/Scripts/ShotGun.cs
+++ Assets/Scripts/ShotGun.cs
@@ -3,15 +3,18 @@ using System.Collections;
 
 public class ShotGun : MonoBehaviour {
 
+	Animator animator;
+
 	// Use this for initialization
 	void Start () {
-	
+		animator = GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown (KeyCode.Space)) {
 			print ("fire!");
+			animator.SetTrigger ("Gazed");
 		}
 	}
 }
