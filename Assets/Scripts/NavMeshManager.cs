395e9b4b525336231bc931b43e6bff8901172685
kang yoosam <yooska14@gmail.com>
2016-11-07 02:16:12 +0900

    [add]Nav Mesh

---
+++ Assets/Scripts/NavMeshManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NavMeshManager : MonoBehaviour {
+
+	public GameObject target;
+
+	// Use this for initialization
+	void Start () {
+		NavMeshAgent agent = GetComponent<NavMeshAgent> ();
+		agent.speed = 1;
+		agent.destination = target.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
